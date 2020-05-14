2da37b22d9f2b944c42dd9af0be88bc5e653fdf7
palepunk <janos@plugdio.com>
2020-05-14 13:59:53 +0000

    basic status page

--- src/app/presenters/status.php
+++ src/app/presenters/status.php
@@ -19,7 +19,15 @@ class Status {
 		$svApi = new \Supervisor\Api('127.0.0.1', 9001 /* username, password */);
 
 		$status = new \stdClass();
-		$status->supervisorApiVersion = $svApi->getApiVersion();
+#		$status->supervisorApiVersion = $svApi->getApiVersion();
+		$this->l->debug($this->tr . " - " . __METHOD__ . " - " . print_r($svApi->getState(), true));
+#		$status->states = array();
+		$status->processes = array();
+		$status->supervisord = $svApi->getState()["statename"];
+		foreach (array('php-fpm:php-fpm_00', 'mqtt-connector', 'sessionmanager') as $name) {
+#			$status->states[$name] = $svApi->getProcessInfo($name)["statename"];
+			$status->processes[] = $svApi->getProcessInfo($name);
+		}
 
 		header('Content-Type: application/json');
 		echo json_encode($status, JSON_PRETTY_PRINT);
