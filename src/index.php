52f9b954d02291e034cee4181c43d147870bdad5
palepunk <janos@plugdio.com>
2023-01-03 12:43:26 +0000

    Moving from GA to GTM

--- src/index.php
+++ src/index.php
@@ -31,7 +31,7 @@ if (rtrim(strtoupper(getenv('STATUSLIGHT_ENV'))) == 'DEV') {
     $f3->set('ENV', 'DEV');
     $f3->set('baseAppPath', 'http://' . trim(getenv('DOMAIN')));
 } else {
-    $f3->set('GOOGLE_AD_ID', strtoupper(getenv('GOOGLE_AD_ID')));
+    $f3->set('GOOGLE_TAG_ID', strtoupper(getenv('GOOGLE_TAG_ID')));
     $f3->set('ENV', strtoupper(getenv('STATUSLIGHT_ENV')));
     $f3->set('baseAppPath', 'https://' . trim(getenv('DOMAIN')));
 }
